5de18d9d783a0f6b2f6aeb98a0d5dd452a3b3761
tim <tim@timschupp.de>
2023-09-17 19:52:17 +0200

    finished modal operator, it can now fetch and execute tasks

--- blender_update_operator.py
+++ blender_update_operator.py
@@ -1,41 +1,68 @@
 import bpy
 from bpy.props import IntProperty, FloatProperty
 import requests
+import time
 
 
+def execute_task(task):
+    print("Executing task: ", task)
+    if task["type"] == "run_script":
+        filename = task["script_path"]
+        exec(compile(open(filename).read(), filename, 'exec'))
+
+    
 class ModalOperator(bpy.types.Operator):
     """
     Tim's Blender socker communicator
     """
     
-    bl_idname = "object.modal_operator"
+    bl_idname = "wm.modal_timer_operator"
     bl_label = "Tim's Blender BUS"
 
     first_mouse_x: IntProperty()
     first_value: FloatProperty()
+    
+    prev_time = None
 
     def modal(self, context, event):
         
-        if event.type in {'ESC'}:
+        if event.type in {'RIGHTMOUSE', 'ESC'}:
+            self.cancel(context)
             return {'CANCELLED'}
 
+        if event.type == 'TIMER':
+            if self.prev_time is None:
+                self.prev_time = time.time()
+            else:
+                passed_time = abs(self.prev_time - time.time())
+                if passed_time > 1.0:
+                    self.prev_time = time.time()
+                    response = requests.get('http://localhost:5000/tasks/')
+                    tasks = response.json()
+                    if len(tasks) > 0:
+                        print("NEW TASK DETECTED")
+                        for task in tasks:
+                            execute_task(task)
+
+        return {'PASS_THROUGH'}
+    
+    def execute(self, context):
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.1, window=context.window)
+        wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
-    def invoke(self, context, event):
-        # Always fetch the dev server `localhost:8128/tasks`
-        
-        response = requests.post('http://localhost:8128/tasks')
-        print(response.json())
-
-        
-        return {'RUNNING_MODAL'}
-
-
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
+    
 def menu_func(self, context):
     self.layout.operator(ModalOperator.bl_idname, text=ModalOperator.bl_label)
+    
 
 
 def register():
+    
     bpy.utils.register_class(ModalOperator)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
@@ -49,5 +76,4 @@ if __name__ == "__main__":
     register()
     
     print("Starting modal operator, press ESC to quit.")
-
-    bpy.ops.object.modal_operator('INVOKE_DEFAULT')
+    bpy.ops.wm.modal_timer_operator()
